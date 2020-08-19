3bb7d432f60594333f9ca1aad7c4876725b2eac9
Praveen Kumaravel <praveen1994kp@gmail.com>
2020-08-19 12:12:04 +0530

    Login form modal

--- assets/js/fitnessapp.js
+++ assets/js/fitnessapp.js
@@ -1,28 +1,28 @@
-const isVisible = "is-visible";
+const isVisible = "modal-active";
 
 function openUserModal() {
-  let modal = document.getElementById("modal-signup");
+  let modal = document.getElementById("user-modal");
 
   modal.classList.add(isVisible);
 }
 
 function modalCloseHandler() {
-  let closeBtn = document.getElementById("close-modal");
+  let closeBtn = document.getElementById("close-icon");
   closeBtn.addEventListener("click", function () {
-    let modal = document.getElementById("modal-signup");
+    let modal = document.getElementById("user-modal");
     modal.classList.remove(isVisible);
   });
 
   document.addEventListener("click", (e) => {
-    if (e.target == document.querySelector(".modal.is-visible")) {
-      let modal = document.getElementById("modal-signup");
+    if (e.target == document.querySelector(".modal-bg.modal-active")) {
+      let modal = document.getElementById("user-modal");
       modal.classList.remove(isVisible);
     }
   });
 
   document.addEventListener("keyup", (e) => {
-    if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
-      let modal = document.getElementById("modal-signup");
+    if (e.key == "Escape" && document.querySelector(".modal-bg.modal-active")) {
+      let modal = document.getElementById("user-modal");
       modal.classList.remove(isVisible);
     }
   });
