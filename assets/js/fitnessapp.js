2db94e9b38e357295c2dfb4fc71c6fb30702cce2
Praveen Kumaravel <praveen1994kp@gmail.com>
2020-08-19 18:37:34 +0530

    signup/login nav

--- assets/js/fitnessapp.js
+++ assets/js/fitnessapp.js
@@ -1,4 +1,8 @@
 const isVisible = "modal-active";
+const loginCtaId = "login-cta";
+const signUpCtaId = "signup-cta";
+const loginFormContainerId = "login-form-container";
+const signUpFormContainerId = "signup-form-container";
 
 function openUserModal() {
   let modal = document.getElementById("user-modal");
@@ -11,12 +15,14 @@ function modalCloseHandler() {
   closeBtn.addEventListener("click", function () {
     let modal = document.getElementById("user-modal");
     modal.classList.remove(isVisible);
+    resetUserForm();
   });
 
   document.addEventListener("click", (e) => {
     if (e.target == document.querySelector(".modal-bg.modal-active")) {
       let modal = document.getElementById("user-modal");
       modal.classList.remove(isVisible);
+      resetUserForm();
     }
   });
 
@@ -24,8 +30,47 @@ function modalCloseHandler() {
     if (e.key == "Escape" && document.querySelector(".modal-bg.modal-active")) {
       let modal = document.getElementById("user-modal");
       modal.classList.remove(isVisible);
+      resetUserForm();
     }
   });
 }
 
 modalCloseHandler();
+
+function setupFormNavActions(ctaId, formId, others) {
+  let selectorEl = document.getElementById(ctaId);
+  selectorEl.addEventListener("click", function () {
+    others.forEach((idToBeRemoved) => {
+      const elementToBeRemoved = document.getElementById(idToBeRemoved);
+      elementToBeRemoved.classList.remove("active");
+    });
+    let formEl = document.getElementById(formId);
+    const isAlreadyActive = formEl.classList.contains("active");
+    !isAlreadyActive && formEl.classList.add("active");
+  });
+}
+
+function resetUserForm() {
+  const signUpActive = document
+    .getElementById(signUpFormContainerId)
+    .classList.contains("active");
+  if (signUpActive) {
+    document.getElementById(signUpFormContainerId).remove("active");
+    document.getElementById(loginFormContainerId).classList.add("active");
+  }
+}
+
+function userNavBarActions() {
+  setupFormNavActions(
+    loginCtaId,
+    loginFormContainerId,
+    new Array(signUpFormContainerId)
+  );
+  setupFormNavActions(
+    signUpCtaId,
+    signUpFormContainerId,
+    new Array(loginFormContainerId)
+  );
+}
+
+userNavBarActions();
